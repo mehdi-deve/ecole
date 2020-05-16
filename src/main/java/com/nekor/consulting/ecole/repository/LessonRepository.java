8e459fac417cacfb492e46fde14d860825119c1b
Mehdi01 <diourimehdi01@gmail.com>
2020-05-16 18:27:40 +0200

    Update LessonRepository.java

--- src/main/java/com/nekor/consulting/ecole/repository/LessonRepository.java
+++ src/main/java/com/nekor/consulting/ecole/repository/LessonRepository.java
@@ -6,9 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface LessonRepository extends JpaRepository<Lesson, String>, JpaSpecificationExecutor<Lesson> {
     @Query(value = "SELECT * FROM ecole.cours where ecole.cours.classe_id=?1", nativeQuery = true)
-    public List<Lesson> getLessonByClasse_id(String classe_id);
+    public List<Lesson> getLessonByClasseId(String classe_id);
+
+    @Query(value = "SELECT * FROM ecole.cours WHERE ecole.cours.date_creation > CURDATE()", nativeQuery = true)
+    List<Lesson> getNextCours();
+
+    @Query(value = "SELECT * FROM ecole.cours WHERE ecole.cours.date_creation < CURDATE()", nativeQuery = true)
+    List<Lesson> getLastCours();
 }
