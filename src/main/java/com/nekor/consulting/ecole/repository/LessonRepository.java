eed53c996dee16ad70355886ec88bb4fe125ead3
Mehdi01 <diourimehdi01@gmail.com>
2020-05-16 07:34:23 +0200

    Update LessonRepository.java

--- src/main/java/com/nekor/consulting/ecole/repository/LessonRepository.java
+++ src/main/java/com/nekor/consulting/ecole/repository/LessonRepository.java
@@ -1,9 +1,14 @@
 package com.nekor.consulting.ecole.repository;
 
+import com.nekor.consulting.ecole.model.ClassRoom;
 import com.nekor.consulting.ecole.model.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LessonRepository extends JpaRepository<Lesson, String>, JpaSpecificationExecutor<Lesson> {
-
+    @Query(value = "SELECT * FROM ecole.cours where ecole.cours.classe_id=?1", nativeQuery = true)
+    public List<Lesson> getLessonByClasse_id(String classe_id);
 }
