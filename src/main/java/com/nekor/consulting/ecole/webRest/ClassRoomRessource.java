af0f20e2c61cf5225f482dc146ff7d41af1e9005
Mehdi01 <diourimehdi01@gmail.com>
2020-05-16 07:33:29 +0200

    Update ClassRoomRessource.java

--- src/main/java/com/nekor/consulting/ecole/webRest/ClassRoomRessource.java
+++ src/main/java/com/nekor/consulting/ecole/webRest/ClassRoomRessource.java
@@ -4,8 +4,11 @@ package com.nekor.consulting.ecole.webRest;
 import com.nekor.consulting.ecole.model.ClassRoom;
 import com.nekor.consulting.ecole.repository.ClassRoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -33,5 +36,22 @@ public class ClassRoomRessource {
     private Optional<ClassRoom> findClassRoomById(@RequestParam(name = "classRoomId") String classRoomId) {
         return  classRoomRepository.findById(classRoomId);
     }
+
+    @GetMapping("/etablissement")
+    private ResponseEntity<List<ClassRoom>> findClassroomById(@RequestParam(name = "id") String id) {
+        return ResponseEntity.
+                ok().
+                contentType(MediaType.APPLICATION_JSON).
+                body( classRoomRepository.getClassRoomByEtablissement_id(id));
+    }
+
+    @GetMapping("/utilisateur")
+    private ResponseEntity<List<ClassRoom>> getClassRoomByUser_id(@RequestParam(name = "id") String id) {
+        return ResponseEntity.
+                ok().
+                contentType(MediaType.APPLICATION_JSON).
+                body( classRoomRepository.getClassRoomByUser_id(id));
+    }
+
 
 }
