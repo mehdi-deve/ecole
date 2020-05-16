8d99f24569ba214b8405c345c6cd0a1bbabfd854
Mehdi01 <diourimehdi01@gmail.com>
2020-05-16 07:31:35 +0200

    Update ClassRoomRepository.java

--- src/main/java/com/nekor/consulting/ecole/repository/ClassRoomRepository.java
+++ src/main/java/com/nekor/consulting/ecole/repository/ClassRoomRepository.java
@@ -3,7 +3,16 @@ package com.nekor.consulting.ecole.repository;
 import com.nekor.consulting.ecole.model.ClassRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ClassRoomRepository extends JpaRepository<ClassRoom, String>, JpaSpecificationExecutor<ClassRoom> {
+
+    @Query(value = "SELECT * FROM ecole.classe where ecole.classe.etablissement_id=?1", nativeQuery = true)
+    public List<ClassRoom> getClassRoomByEtablissement_id(String etablissement_id);
+
+    @Query(value = "SELECT * FROM ecole.classe where ecole.classe.user_id=?1", nativeQuery = true)
+    public List<ClassRoom> getClassRoomByUser_id(String user_id);
 
 }
