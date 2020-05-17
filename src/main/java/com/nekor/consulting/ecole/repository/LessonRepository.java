7a55e1d232c2daa287256244aac3147252ae4876
Mehdi01 <diourimehdi01@gmail.com>
2020-05-17 17:56:13 +0200

    Update LessonRepository.java

--- src/main/java/com/nekor/consulting/ecole/repository/LessonRepository.java
+++ src/main/java/com/nekor/consulting/ecole/repository/LessonRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public interface LessonRepository extends JpaRepository<Lesson, String>, JpaSpecificationExecutor<Lesson> {
@@ -23,9 +24,17 @@ public interface LessonRepository extends JpaRepository<Lesson, String>, JpaSpec
             "AND ecole.eleve.id=?1 AND ecole.cours.matiere_id=?2",nativeQuery = true)
     public List<Lesson> getLessonByEleveIdAndMatiereId(String eleve_id, String matiere_id);
 
+    @Query(value="SELECT * FROM ecole.cours, ecole.eleve WHERE ecole.cours.classe_id=ecole.eleve.classe_id " +
+            "AND ecole.eleve.id=?1 AND ecole.cours.creationDate=?2",nativeQuery = true)
+    public List<Lesson> getLessonByEleveIdAndDate(String eleve_id, Date creationDate);
+
+    @Query(value="SELECT * FROM ecole.cours, ecole.eleve WHERE ecole.cours.classe_id=ecole.eleve.classe_id " +
+            "AND ecole.eleve.id=?1 AND ecole.cours.matiere_id=?2 AND ecole.cours.creationDate=?3",nativeQuery = true)
+    public List<Lesson> getLessonByEleveIdAndMatiereIdAndDate(String eleve_id, String matiere_id, Date creationDate);
+
     @Query(value = "SELECT * FROM ecole.cours WHERE ecole.cours.date_creation > CURDATE()", nativeQuery = true)
-    List<Lesson> getNextCours();
+    public List<Lesson> getNextCours();
 
     @Query(value = "SELECT * FROM ecole.cours WHERE ecole.cours.date_creation < CURDATE()", nativeQuery = true)
-    List<Lesson> getLastCours();
+    public List<Lesson> getLastCours();
 }
